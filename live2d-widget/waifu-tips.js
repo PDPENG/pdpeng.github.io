37f9892baa6ac82a58d557842b472ba36690933c
Jason <jason.peng.code@gmail.com>
2022-05-14 15:33:33 +0800

    Site updated: 2022-05-14 15:32:58

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -77,7 +77,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("https://github.com/PDPENG/live2d-widget");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
